1de49a588ada0670cdbd0973a179111e65310fdd
Miro Markaravanes <miro.markaravanes@gmail.com>
2016-10-22 10:13:03 -0700

    Initial Commit

---
+++ MinusMinus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinusMinus
+//
+//  Created by Miro Markarian on 10/22/16.
+//  Copyright © 2016 GCC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
